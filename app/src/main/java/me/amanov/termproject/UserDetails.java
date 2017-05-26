d9168cce7e4e59c0ccb97bd633c82a5a3db6b1e5
Dovlet Amanov <turkmenium@gmail.com>
2017-05-26 17:03:09 +0300

    Initial commit

---
+++ app/src/main/java/me/amanov/termproject/UserDetails.java
@@ -0,0 +1,10 @@
+package me.amanov.termproject;
+
+/**
+ * Created by Dovik on 5/24/2017.
+ */
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
